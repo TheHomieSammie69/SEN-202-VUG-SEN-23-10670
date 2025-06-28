8a02464b652e93a60bf6c05e3c00deaed2eaf08d
TheHomieSammie69 <texamidakwo@gmail.com>
2025-06-28 10:17:55 +0100

    Create urls.py

---
+++ SAMUEL_SEN202/staff_api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ManagerViewSet,InternViewSet
+
+router= DefaultRouter()
+router.register(r'managers',ManagerViewSet)
+router.register(r'Interns',InternViewSet
+                urlpatterns = [
+                  path('',include(router.urls)).
+                ]
